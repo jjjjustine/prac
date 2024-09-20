2251462a11ce83c1d64be0d059d5260dd2424bfd
jjjjustine <catingan.janejustine@gmail.com>
2024-09-20 18:48:48 +0800

    added

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+Person p = new Person();
+
+p.firstName = "Justine";
+p.lastName = "Catingan";
+p.sex = 'F';
+p.age = 18;
+
+System.out.println(p.firstName);
+
+    }
+    
+}
